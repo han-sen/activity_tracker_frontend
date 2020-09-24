6999bff2b99cb5e732bbf48cd06cf02b4e07522a
Michael Hansen <michaelandhansen@gmail.com>
2020-09-24 11:46:53 -0400

    index with chart finished

---
+++ src/components/BarChart.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import Chart from "chart.js";
+
+const BarChart = () => {
+    const prepareData = (data) => {
+        console.log(data);
+        const chartData = {
+            labels: [],
+            datasets: [
+                {
+                    label: "Hours Spent",
+                    data: [],
+                },
+            ],
+        };
+
+        data.forEach((activity) => {
+            chartData.labels.push(activity.description);
+            chartData.datasets[0].data.push(activity.time_spent);
+        });
+        return chartData;
+    };
+    const createChart = (data) => {
+        const ctx = document.querySelector("#activities_chart");
+        return new Chart(ctx, {
+            type: "bar",
+            data: data,
+        });
+    };
+    const getData = () => {
+        fetch("/activities/")
+            .then((response) => response.json())
+            .then((jData) => prepareData(jData))
+            .then((data) => createChart(data))
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <>
+            <h1>Activities / Time Spent</h1>
+            <canvas id="activities_chart" width="300" height="100"></canvas>
+        </>
+    );
+};
+
+export default BarChart;
